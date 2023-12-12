8436fbe2f0f8b4c94c621d4ccb33fd4010e3da5b
TechnicallyCoded <technicallycoded@gmail.com>
2023-12-12 23:18:46 +0100

    Add permission constants class

---
+++ src/main/java/net/devmart/skywarsreloaded/api/utils/properties/PermissionProperties.java
@@ -0,0 +1,16 @@
+package net.devmart.skywarsreloaded.api.utils.properties;
+
+public class PermissionProperties {
+
+    // General Permissions
+    public static final String GENERAL_ALTER_LOBBY = "skywars.alterlobby";
+
+    // Commands
+
+    // Admin
+
+    // Maps
+
+    // Kits
+
+}
