092941fa2b22a1868db5e7839bda583dcfef0d4d
lukasvdgaag <lukasvdgaag@gmail.com>
2023-12-29 14:45:11 +0100

    Added paginated unlockables GUI logic with fixed menu positions.

---
+++ src/main/java/net/devmart/skywarsreloaded/api/utils/gui/SWUnlockablesGui.java
@@ -0,0 +1,31 @@
+package net.devmart.skywarsreloaded.api.utils.gui;
+
+import net.devmart.skywarsreloaded.api.unlockable.Unlockable;
+
+import java.util.List;
+
+public interface SWUnlockablesGui<T extends Unlockable> extends SWPaginatedGui<T> {
+
+    /**
+     * Get all items in this gui, sorted and organized.
+     * This method is used to determine the items per page.
+     *
+     * @return the sorted and organized items
+     */
+    List<T> getAllItems();
+
+    /**
+     * Get all items in this gui, unsorted and unorganized.
+     *
+     * @return the unsorted and unorganized items
+     */
+    List<T> getAllItemsUnsorted();
+
+    /**
+     * Sort and organize the items in this gui.
+     * This method is called automatically when the gui is created
+     * and should be called again if the items are changed.
+     */
+    void sortAndOrganizeItems();
+
+}
---
+++ src/main/java/net/devmart/skywarsreloaded/api/utils/properties/UnlockableProperties.java
@@ -0,0 +1,11 @@
+package net.devmart.skywarsreloaded.api.utils.properties;
+
+public class UnlockableProperties {
+
+    public static final String MENU_POSITION = "menu-position";
+
+    public static final String REQUIREMENTS_PERMISSION = "requirements.permission";
+    public static final String REQUIREMENTS_COST = "requirements.cost";
+    public static final String REQUIREMENTS_STATS = "requirements.stats";
+
+}
