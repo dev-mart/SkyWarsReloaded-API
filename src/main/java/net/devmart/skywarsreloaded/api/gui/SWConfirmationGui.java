5bea790da962aa8fc98384a98c332e13040278f6
lukasvdgaag <lukasvdgaag@gmail.com>
2024-02-20 14:40:59 +0100

    Simplified the AbstractSWUnlockablesGui class.

--- src/main/java/net/devmart/skywarsreloaded/api/gui/SWConfirmationGui.java
+++ src/main/java/net/devmart/skywarsreloaded/api/gui/SWConfirmationGui.java
@@ -1,11 +1,19 @@
 package net.devmart.skywarsreloaded.api.gui;
 
 import net.devmart.skywarsreloaded.api.gui.handlers.SWGuiClickHandler;
+import net.devmart.skywarsreloaded.api.unlockable.Unlockable;
+import net.devmart.skywarsreloaded.api.utils.Item;
+
+import java.util.function.Function;
 
 public interface SWConfirmationGui extends SWGui {
 
-    void addConfirmButton(String configMessageProperty, SWGuiClickHandler handler);
+    void addConfirmButton(Unlockable unlockable, SWGuiClickHandler handler);
 
-    void addCancelButton(String configMessageProperty, SWGuiClickHandler handler);
+    void addConfirmButton(Function<Item, Item> itemModifier, SWGuiClickHandler handler);
+
+    void addCancelButton(Unlockable unlockable, SWGuiClickHandler handler);
+
+    void addCancelButton(Function<Item, Item> itemModifier, SWGuiClickHandler handler);
 
 }
