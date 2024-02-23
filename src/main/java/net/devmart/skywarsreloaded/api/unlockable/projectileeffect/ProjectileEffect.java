36bebfdd1ee0d860b48e40d6fdd5fc4129a65447
lukasvdgaag <lukasvdgaag@gmail.com>
2024-02-24 00:19:50 +0100

    Added logic for showing projectile effects during games.

--- src/main/java/net/devmart/skywarsreloaded/api/unlockable/projectileeffect/ProjectileEffect.java
+++ src/main/java/net/devmart/skywarsreloaded/api/unlockable/projectileeffect/ProjectileEffect.java
@@ -2,15 +2,15 @@ package net.devmart.skywarsreloaded.api.unlockable.projectileeffect;
 
 import net.devmart.skywarsreloaded.api.unlockable.Unlockable;
 import net.devmart.skywarsreloaded.api.wrapper.entity.SWEntity;
-import net.devmart.skywarsreloaded.api.wrapper.entity.SWPlayer;
+import net.devmart.skywarsreloaded.api.wrapper.entity.SWProjectile;
 import net.devmart.skywarsreloaded.api.wrapper.world.block.SWBlock;
 
 public interface ProjectileEffect extends Unlockable {
 
-    void tick(SWEntity entity);
+    void tick(SWProjectile entity);
 
-    void onPlayerHit(SWEntity projectile, SWPlayer player);
+    void onEntityHit(SWProjectile projectile, SWEntity hitEntity);
 
-    void onBlockHit(SWEntity projectile, SWBlock block);
+    void onBlockHit(SWProjectile projectile, SWBlock block);
 
 }
--- src/main/java/net/devmart/skywarsreloaded/api/wrapper/event/SWProjectileHitEvent.java
+++ src/main/java/net/devmart/skywarsreloaded/api/wrapper/event/SWProjectileHitEvent.java
@@ -2,12 +2,13 @@ package net.devmart.skywarsreloaded.api.wrapper.event;
 
 import net.devmart.skywarsreloaded.api.event.SWEvent;
 import net.devmart.skywarsreloaded.api.wrapper.entity.SWEntity;
+import net.devmart.skywarsreloaded.api.wrapper.entity.SWProjectile;
 import net.devmart.skywarsreloaded.api.wrapper.world.block.SWBlock;
 import org.jetbrains.annotations.Nullable;
 
 public interface SWProjectileHitEvent extends SWEvent, SWCancellable{
 
-    SWEntity getProjectile();
+    SWProjectile getProjectile();
 
     @Nullable
     SWBlock getHitBlock();
